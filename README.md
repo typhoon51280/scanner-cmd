ba09e738a6cf3ab26f871d6d0d2926a3611e6d0e
typhoon51280 <typhoon51280@users.noreply.github.com>
2022-03-25 19:08:18 +0100

    handling parse mode

--- README.md
+++ README.md
@@ -8,8 +8,55 @@ USAGE:
     scanner-cmd.exe [OPTIONS] --code `<code>`
 ```
 OPTIONS:
-    -c, --code <code>      Qr Code
+    -c, --code <code>      Inline Text Input (single quote): 'ipselorumdixit'
     -d, --delay <delay>    Delay before input simulation start
+    -f, --file <file>      Filename Text Input
     -h, --help             Print help information
+    -p, --parse <parse>    Parse mode enabled
     -V, --version          Print version information
+```
+
+## Parse Mode
+
+### ***Disabled*** (default)
+
+The text is entered as-is, no handling of special keys (SHIFT,ALT,...).
+
+### ***Enabled***
+
+The text is parsed before keyboard sequence is entered.
+
+The following brackets are available:
+
+- UNICODE
+- CTRL
+- SHIFT
+- META
+- ALT
+
+```
+{+UNICODE}Hello world{-UNICODE}
+{+CTRL}a{-CTRL}
+{+SHIFT}Hello World{-SHIFT}
+{+META}Hello World{-META}
+{+ALT}Hello World{-ALT}
+```
+
+Thera are also special conversion of the following characters:
+```
+- {CR} => \r
+- {LF} => \n
+```
+
+Examples:
+
+```
+{+SHIFT}hello{CR}{LF}world{-SHIFT}
+```
+
+is rendered as 
+
+```
+HELLO
+WORLD
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use clap::{App, Arg};
-use enigo::{Enigo, KeyboardControllable};
 use std::{thread, time::Duration};
+use std::fs;
+use enigo::{Enigo, KeyboardControllable};
 
 fn main() {
     let matches = App::new("Keyboard Emulator")
@@ -13,19 +14,57 @@ fn main() {
                 .long("code")
                 .takes_value(true)
                 .required(true)
-                .help("Qr Code"),
+                .forbid_empty_values(true)
+                .conflicts_with("file")
+                .help("Inline Text Input (single quote): 'ipselorumdixit'"),
+        )
+        .arg(
+            Arg::new("file")
+                .short('f')
+                .long("file")
+                .takes_value(true)
+                .required(true)
+                .forbid_empty_values(true)
+                .conflicts_with("code")
+                .help("Filename Text Input"),
         )
         .arg(
             Arg::new("delay")
                 .short('d')
                 .long("delay")
                 .takes_value(true)
+                .required(false)
+                .forbid_empty_values(true)
+                .default_missing_value("0")
                 .help("Delay before input simulation start"),
         )
+        .arg(
+            Arg::new("parse")
+                .short('p')
+                .long("parse")
+                .takes_value(false)
+                .required(false)
+                .help("Parse Mode enabled"),
+        )
         .get_matches();
-    let code = matches.value_of("code").unwrap_or("");
-    let delay = matches.value_of("delay").unwrap_or("0").parse::<u64>().unwrap_or(0);
+    let delay = matches.value_of("delay").unwrap().parse::<u64>().unwrap_or(0);
+    let parse_flag = matches.is_present("parse");
+    let file = matches.value_of("file").unwrap_or("");
+    let content: String;
+    if !file.is_empty() {
+        content = fs::read_to_string(file).expect("error reading 'file' contents");
+    } else {
+        content = matches.value_of("code").unwrap().to_string();
+    }
+    let command = Some(content.trim_matches(|c| c == '\'')).expect("error trim code");
     let mut enigo = Enigo::new();
     thread::sleep(Duration::from_secs(delay));
-    enigo.key_sequence(Some(code.trim_matches(|c| c == '\'')).unwrap_or(""));
+    if parse_flag {
+        let command_unicode = command
+            .replace("{CR}", "\r")
+            .replace("{LF}", "\n");
+        enigo.key_sequence_parse(command_unicode.as_str());
+    } else {
+        enigo.key_sequence(command);
+    }
 }
